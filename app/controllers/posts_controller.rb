e8f1d138ef68adeb6782be44ab4f909004f30209
Jacob Serres <jserres@zappos.com>
2013-06-24 14:55:15 -0700

    Tester

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end	
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post])
+
+		if @post.save
+			redirect_to posts_path, :notice => "Post Saved"
+		else
+			render "new"
+		end
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		if @post.destroy
+			redirect_to posts_path, :notice => "Post Deleted"
+		else
+			render "Post not deleted"
+		end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddit::Application.config.secret_token = 'da654715e42ae53ac570934b0c5f9a9d1d55a5ec17d71be2a7e30b2aef1e2e275c434f9648135cbbd444eed046f9b7a5bc50e49313378d14af5c0f41b154c6e4'
